dd5276de7bcd2f7d6a1c1814f3ce7248967bbede
quentin <quentin.degiorgio@mymail.champlain.edu>
2021-03-10 14:23:32 -0500

    secure ssh script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,11 +3,14 @@
 #!/bin/bash
 
 #creates a new ssh user using the $1 paramter
-sudo useradd testuser $1
-
 #adds a public key from the local repo or curled from the remote repo
-curl -LJO https://raw.githubusercontent.com/QuentinD901/SYS265-tech-journal/main/linux/public-keys/id_rsa.pub
-ssh-copy-id testuser@$HOSTNAME
+sudo useradd -m -d /home/testuser -s /bin/bash testuser $1
+sudo mkdir /home/testuser/.ssh
+sudo cp ~/SYS265-tech-journal/linux/public-keys/id_rsa.pub /home/testuser/.ssh/authorized_keys
+sudo chmod 700 /home/testuser/.ssh
+sudo chmod 600 /home/testuser/.ssh/authorized_keys
+sudo chown -R testuser:testuser /home/testuser/.ssh
+
 
 #removes root's ssh ability
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
