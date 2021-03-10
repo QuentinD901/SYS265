bb23ebb8b28d9be35b91480c95c82888d2da2078
quentin <quentin.degiorgio@mymail.champlain.edu>
2021-03-10 14:28:04 -0500

    secure ssh script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -6,7 +6,7 @@
 #adds a public key from the local repo or curled from the remote repo
 sudo useradd -m -d /home/testuser -s /bin/bash testuser $1
 sudo mkdir /home/testuser/.ssh
-sudo cp ~/SYS265-tech-journal/linux/public-keys/id_rsa.pub /home/testuser/.ssh/authorized_keys
+sudo cp /SYS265-tech-journal/linux/public-keys/id_rsa.pub /home/testuser/.ssh/authorized_keys
 sudo chmod 700 /home/testuser/.ssh
 sudo chmod 600 /home/testuser/.ssh/authorized_keys
 sudo chown -R testuser:testuser /home/testuser/.ssh
