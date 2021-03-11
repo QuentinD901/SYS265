aadcd6d49ec80d6191e798bbee4883ea6479426b
quentin <quentin.degiorgio@mymail.champlain.edu>
2021-03-11 01:10:30 +0000

    updated chungus user ssh script

--- linux/centos7/chungus-secure-ssh.sh
+++ linux/centos7/chungus-secure-ssh.sh
@@ -3,7 +3,7 @@
 #creates a new ssh user using the $1 paramter
 sudo useradd -m -d /home/chungus -s /bin/bash chungus
 sudo mkdir /home/chungus/.ssh
-sudo cp SYS265-tech-journal/linux/public-keys/id_rsa.pub
+sudo cp /home/quentin/SYS265-tech-journal/linux/public-keys/id_rsa.pub /home/chungus/.ssh/authorized_keys
 sudo chmod 700 /home/chungus/.ssh
 sudo chmod 600 /home/chungus/.ssh/authorized_keys
 sudo chown -R chungus:chungus /home/chungus/.ssh
