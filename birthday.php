660430f43d4ccede9a8d9873f508de8bdd2b7235
QuentinD901 <59927935+QuentinD901@users.noreply.github.com>
2021-04-28 22:24:07 -0400

    Update birthday.php

--- birthday.php
+++ birthday.php
@@ -18,16 +18,27 @@
 //Run query - result is reurned as a resource id
 //If query has error, _LINE_ will print the error from mysql
   $result=$mysqli->query($query) or die($mysqli->error.__LINE__);
-
-
-//Resource id results must be iterpreted
-//This while loop will run thru the results row by row & echo name & birth fields
-if ($result->num_rows > 0) {
-  while($row=$result->fetch_assoc()) {
-  echo $row['name']."'s birthday is ".$row['birth']."</br>";
-  }
-  }
+  
+  $stmt=$mysqli->prepare("SELECT birth from cats where name=?");
+  $stmt->bind_param('s',$variable that you set from the $_POST);
+  $stmt->execute();
+  $stmt->bind_result($birth);   //This creates the variable with the birthday result
+  $stmt->store_result();  //After the bind_result so the script knows how many rows are returned.
+  if ($stmt->num_rows >0) {
+  while ($stmt->fetch()) {
+  echo $name."'s birthday is ".$birth;
   else {
-  echo 'NO RESULTS';
+  echo 'No Results';
   }
 ?>
+// FOR NON SEC
+//Resource id results must be iterpreted
+//This while loop will run thru the results row by row & echo name & birth fields
+//if ($result->num_rows > 0) {
+  //while($row=$result->fetch_assoc()) {
+  //echo $row['name']."'s birthday is ".$row['birth']."</br>";
+ // }
+ // }
+  //else {
+  //echo 'NO RESULTS';
+  //}
